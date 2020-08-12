6aaf20f59e82fd36272906a8e48635b8738453c5
Kevin Smith <kev_bite@msn.com>
2020-08-12 20:03:25 +0100

    Rotating face

--- RubiksCube/UnitTest1.cs
+++ RubiksCube/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices.ComTypes;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Xunit;
@@ -47,6 +49,19 @@ namespace RubiksCube
 
         public Cube RotateFront(Rotation direction)
         {
+            //         W W W                           W W W         
+            //         W W W                           W W W         
+            //         B O G                           O B G         
+            //         - - -                           - - -         
+            // G G G | R R R | R B B           G G O | R R R | B B B 
+            // G G B | R R R | G B B     =>>   G G R | R R R | O B B 
+            // G G O | R R R | B B B           G G G | R R R | G B B 
+            //         - - -                           - - -         
+            //         O R G                           B G R         
+            //         Y Y Y                           Y Y Y         
+            //         Y Y Y                           Y Y Y         
+            //         - - -                           - - -        
+
             if (direction == Rotation.Anticlockwise)
             {
                 var front = new Face(Front.TopRight, Front.MiddleRight, Front.BottomRight,
@@ -61,8 +76,18 @@ namespace RubiksCube
                 var front = new Face(Front.BottomLeft, Front.MiddleLeft, Front.TopLeft,
                     Front.BottomMiddle, Front.MiddleMiddle, Front.TopMiddle,
                     Front.BottomRight, Front.MiddleRight, Front.TopRight);
+                var left = new Face(Left.TopLeft, Left.TopMiddle, Bottom.TopLeft, 
+                    Left.MiddleLeft, Left.MiddleMiddle, Bottom.TopMiddle,
+                    Left.BottomLeft, Left.BottomMiddle, Bottom.TopRight);
+                var right = new Face(Top.BottomLeft, Right.TopMiddle, Right.TopRight,
+                    Top.BottomMiddle, Right.MiddleMiddle, Right.MiddleRight,
+                    Top.BottomRight, Right.BottomMiddle, Right.BottomRight);
+                var top = Top.WithBottomRow(Left.BottomRight, Left.MiddleRight, Left.TopRight);
+                var bottom = new Face(Right.BottomLeft, Right.MiddleLeft, Right.TopLeft,
+                    Bottom.MiddleLeft, Bottom.MiddleMiddle, Bottom.MiddleRight,
+                    Bottom.BottomLeft, Bottom.BottomMiddle, Bottom.BottomRight);
 
-                return new Cube(front, Back, Left, Right, Top, Bottom, Rotations + 1u);
+                return new Cube(front, Back, left, right, top, bottom, Rotations + 1u);
             }
         }
     }
@@ -97,6 +122,13 @@ namespace RubiksCube
         public Colour BottomLeft { get; }
         public Colour BottomMiddle { get; }
         public Colour BottomRight { get; }
+
+        public Face WithBottomRow(Colour left, Colour middle, Colour right)
+        {
+            return new Face(TopLeft, TopMiddle, TopRight,
+                            MiddleLeft, MiddleMiddle, MiddleRight,
+                            left, middle, right);
+        }
     }
 
 
@@ -158,7 +190,7 @@ namespace RubiksCube
         }
 
         [Fact]
-        public void RotateFrontClockwise()
+        public void RotateFrontClockwiseFrontShouldBe()
         {
             /*
                 R G B     R O R 
@@ -181,8 +213,97 @@ namespace RubiksCube
             rotatedCube.Rotations.Should().Be(1);
         }
 
+        [Theory]
+        [InlineData(Rotation.Clockwise)]
+        [InlineData(Rotation.Anticlockwise)]
+        public void RotateFrontClockwiseBackShouldBeSame(Rotation direction)
+        {
+            /*
+                R G B     
+                O G Y  => Same 
+                R W B     
+            */
+
+            var cube = new Cube(new Face(Colour.Red),
+                new Face(Colour.Red, Colour.Green, Colour.Blue,
+                Colour.Orange, Colour.Green, Colour.Yellow,
+                Colour.Red, Colour.White, Colour.Blue), new Face(Colour.Red), new Face(Colour.Red), new Face(Colour.Red), new Face(Colour.Red));
+
+            var rotatedCube = cube.RotateFront(direction);
+
+            using var _ = new AssertionScope();
+            AssertSideIs(rotatedCube.Back,
+                Colour.Red, Colour.Green, Colour.Blue,
+                Colour.Orange, Colour.Green, Colour.Yellow,
+                Colour.Red, Colour.White, Colour.Blue);
+
+            rotatedCube.Rotations.Should().Be(1);
+        }
+
         [Fact]
-        public void RotateFrontAntiClockwise()
+        public void RotateFrontClockwiseAdjacentFacesShouldBeRotated()
+        {
+            //         W W W                           W W W         
+            //         W W W                           W W W         
+            //         B O G                           O B G         
+            //         - - -                           - - -         
+            // G G G | R R R | R B B           G G O | R R R | B B B 
+            // G G B | R R R | G B B     =>>   G G R | R R R | O B B 
+            // G G O | R R R | B B B           G G G | R R R | G B B 
+            //         - - -                           - - -         
+            //         O R G                           B G R         
+            //         Y Y Y                           Y Y Y         
+            //         Y Y Y                           Y Y Y         
+            //         - - -                           - - -         
+
+            var front = new Face(Colour.Red);
+            var left = CreateFace("GGGGGBGGO");
+            var right = CreateFace("RBBGBBBBB");
+            var top = CreateFace("WWWWWWBOG");
+            var bottom = CreateFace("ORGYYYYYY");
+            var back = new Face(Colour.Yellow);
+
+            var cube = new Cube(front, back, left, right, top, bottom);
+
+            var rotatedCube = cube.RotateFront(Rotation.Clockwise);
+
+            AssertSideIsComplete(rotatedCube.Front, Colour.Red);
+            AssertSideIsComplete(rotatedCube.Back, Colour.Yellow);
+            rotatedCube.Should().BeEquivalentTo(new
+            {
+                Left = CreateFace("GGOGGRGGG"),
+                Right= CreateFace("BBBOBBGBB"),
+                Top = CreateFace("WWWWWWOBG"),
+                Bottom = CreateFace("BGRYYYYYY")
+            });
+
+            rotatedCube.Rotations.Should().Be(1);
+        }
+
+        public static Face CreateFace(string colours)
+        {
+            if (colours.Length != 9) throw new Exception($"Have {colours.Length} colours for this face ({colours}) instead of nine.");
+
+            Colour ColourFromChar(char c)
+                => c switch
+                {
+                    'R' => Colour.Red,
+                    'G' => Colour.Green,
+                    'B' => Colour.Blue,
+                    'Y' => Colour.Yellow,
+                    'O' => Colour.Orange,
+                    'W' => Colour.White,
+                    _ => throw new NotImplementedException()
+                };
+
+            return new Face(ColourFromChar(colours[0]), ColourFromChar(colours[1]), ColourFromChar(colours[2]),
+                             ColourFromChar(colours[3]), ColourFromChar(colours[4]), ColourFromChar(colours[5]),
+                             ColourFromChar(colours[6]), ColourFromChar(colours[7]), ColourFromChar(colours[8])
+                );
+        }
+
+        [Fact]
+        public void RotateFrontAntiClockwiseThenFrontFaceShouldBeRotated()
         {
             /*
                  R O R    R G B 
